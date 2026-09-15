999c0795a6de5f3acf90701ea2d0989034f04938
Priya Aryav <priya.aryav@gmail.com>
2026-09-16 00:51:01 +0530

    arch

---
+++ teenychat/gpt.py
@@ -0,0 +1,18 @@
+"""
+- decoder only transformer
+- RoPE
+- Pre LN
+- ReLU or other
+- GQA
+- kv cache
+- LayerNorm or RMSNorm decide
+
+"""
+import torch
+import torch.nn as nn
+
+class GPT(nn.Module):
+    pass
+
+if __name__ == "__main__":
+    print("hi")
